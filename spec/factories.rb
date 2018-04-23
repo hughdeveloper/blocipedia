24e26d3e0096e7b5b1feed0ec0419dca2c6ac99b
hughdeveloper <hughdeveloper@gmail.com>
2018-04-23 15:21:45 -0700

    Factory_bot_update
    Update Factory to include infromation for account to automatically be counted as email confirmed = true

--- spec/factories.rb
+++ spec/factories.rb
@@ -4,5 +4,7 @@ FactoryBot.define do
     username "bloc"
     email "bloc@blocipedia.com"
     password "password"
+    confirmed_at { Faker::Time.between(2.days.ago, Date.today, :all) }
+    confirmation_sent_at { Faker::Time.between(2.days.ago, 1.days.ago, :all)}
   end
 end
